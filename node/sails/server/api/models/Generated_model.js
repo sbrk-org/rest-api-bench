a9e0d50109c996ed733bc47ccc8e587c340310e6
Manfred Touron <m@42.am>
2014-12-26 02:27:58 +0100

    sails generate model generated_model attr1:string attr2:string attr3:boolean

---
+++ node/sails/server/api/models/Generated_model.js
@@ -0,0 +1,19 @@
+/**
+* Generated_model.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+
+    attr1 : { type: 'string' },
+
+    attr2 : { type: 'string' },
+
+    attr3 : { type: 'boolean' }
+  }
+};
+
