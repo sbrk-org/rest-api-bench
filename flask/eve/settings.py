1bf174304fd5846e2050760f13461b464486e098
s. rannou <mxs@sbrk.org>
2014-12-28 14:34:05 +0100

    Added schema for users.

--- flask/eve/settings.py
+++ flask/eve/settings.py
@@ -1,1 +1,33 @@
-DOMAIN = {'users': {}}
+# MongoDB Configuration
+
+MONGO_HOST = 'localhost'
+MONGO_PORT = 27017
+MONGO_USERNAME = 'api'
+MONGO_PASSWORD = 'api'
+MONGO_DBNAME = 'api-bench'
+
+# API
+RESOURCE_METHODS = ['GET', 'POST', 'DELETE']
+ITEM_METHODS = ['GET', 'PATCH', 'PUT', 'DELETE']
+
+# /users
+users = {
+    'resource_methods': ['GET', 'POST'],
+    'schema': {
+        'users': {
+            'login': {
+                'type': 'string',
+                'minlength': 3,
+                'maxlength': 10,
+                },
+            'role': {
+                'type': 'list',
+                'allowed': ['participant', 'guest', 'admin']
+                },
+            },
+        },
+    }
+
+DOMAIN = {
+    'users': users,
+    }
