ecc261bc97cfc1f76fb985557735c40444153f7a
Manfred Touron <m@42.am>
2014-12-29 21:33:01 +0100

    Tuning martini

--- go/martini/server.go
+++ go/martini/server.go
@@ -3,18 +3,23 @@ package main
 import "github.com/go-martini/martini"
 
 func main() {
+	// Martini setup
+	r := martini.NewRouter()
 	m := martini.New()
-	m.Use(martini.Recovery())
+
 	m.Use(martini.Logger())
-	//m.Use(martini.Router())
-	//m.use(martini.Static())
+	m.Use(martini.Recovery())
+	m.MapTo(r, (*martini.Routes)(nil))
+	m.Action(r.Handle)
+	//m.use(martini.Static("public"))
 	//m.Use(auth.Basic(AuthToken, ""))
 	//m.Use(MapEncoder)
 
-	r := martini.NewRouter()
+	// Methods
 	r.Get("/", func() string {
 		return "Hello World"
 	})
-	m.Action(r.Handle)
+
+	// Run
 	m.Run()
 }
