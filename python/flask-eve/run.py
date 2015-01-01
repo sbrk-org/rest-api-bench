90dee49582e14bcb9d36afb90e9552cf5efe55be
s. rannou <mxs@sbrk.org>
2015-01-01 20:51:30 +0100

    [flask-eve] Added role based auth.

--- python/flask-eve/run.py
+++ python/flask-eve/run.py
@@ -1,4 +1,18 @@
 from eve import Eve
+from eve.auth import BasicAuth
+from werkzeug.security import check_password_hash
+
+class RolesAuth(BasicAuth):
+    def check_auth(self, username, password, allowed_roles, resource, method):
+        # use Eve's own db driver; no additional connections/resources are used
+        accounts = app.data.driver.db['accounts']
+        lookup = {'username': username}
+        if allowed_roles:
+            # only retrieve a user if his roles match ``allowed_roles``
+            lookup['roles'] = {'$in': allowed_roles}
+        account = accounts.find_one(lookup)
+        return account and check_password_hash(account['password'], password)
+
 
 app = Eve()
 
