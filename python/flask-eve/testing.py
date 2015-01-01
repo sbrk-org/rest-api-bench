08e41cfec6583d52ded2485bb58ff6b287e832e7
s. rannou <mxs@sbrk.org>
2015-01-01 21:03:53 +0100

    [flask-eve] added tests for get /users and get /teams.

--- python/flask-eve/testing.py
+++ python/flask-eve/testing.py
@@ -5,14 +5,25 @@ import requests
 
 url = 'http://localhost:5000'
 
-def index():
+def get_index():
     resp = requests.get(url='{0}/'.format(url), params={})
     data = json.loads(resp.text)
     print('GET / ==> {0}'.format(data))
 
+def get_teams():
+    resp = requests.get(url='{0}/teams'.format(url), params={})
+    data = json.loads(resp.text)
+    print('GET /team ==> {0}'.format(data))
+
+def get_users():
+    resp = requests.get(url='{0}/users'.format(url), params={})
+    data = json.loads(resp.text)
+    print('GET /users ==> {0}'.format(data))
+
 def main():
-    index()
-    pass
+    get_index()
+    get_teams()
+    get_users()
 
 if __name__ == '__main__':
     main()
